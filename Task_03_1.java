a407333e33f0c5f08c110506e297e80b999fb6fd
MariaSav9999 <68650418+MariaSav9999@users.noreply.github.com>
2020-09-30 01:02:15 +0300

    Add files via upload

---
+++ Task_03_1.java
@@ -0,0 +1,42 @@
+package by.htp.les03.main;
+
+public class Task_03_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][]mas = new int [0][0];
+		for(int i = 0; i<mas.length; i++){
+			for(int j = 0; j<mas[i].length; j++){			
+			    System.out.print(mas[i][j] + " ");
+			}
+			System.out.println();
+	}
+	{
+		int [][]mas1 = new int [1][1];
+		for(int i = 0; i<mas1.length; i++){
+			for(int j = 0; j<mas1[i].length; j++){			
+			    System.out.print(mas1[i][j] + " ");
+			}
+			System.out.println();
+	}
+	{
+		int [][]mas2 = new int [2][2];
+		for(int i = 0; i<mas2.length; i++){
+			for(int j = 0; j<mas2[i].length; j++){			
+			    System.out.print(mas2[i][j] + " ");
+			}
+			System.out.println();
+	}
+	{
+		int [][]mas3 = new int [3][3];
+		for(int i = 0; i<mas3.length; i++){
+			for(int j = 0; j<mas3[i].length; j++){			
+			    System.out.print(mas3[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	}
+	}
+	}
+}
