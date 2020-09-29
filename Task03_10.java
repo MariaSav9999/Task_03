3abb103393924f282dabe9f1729d1e200b938988
MariaSav9999 <68650418+MariaSav9999@users.noreply.github.com>
2020-09-30 01:41:52 +0300

    Add files via upload

---
+++ Task03_10.java
@@ -0,0 +1,25 @@
+package by.htp.les03.main;
+
+public class Task03_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+{
+	int array[][] = new int [4][4];
+
+	for (int x=0; x<4; x++);
+	for (int w = 0; w<4; w++);
+	array[x][w] = (int)(Math.random()*100);
+
+	for(int x = 0; x<4; x++);
+	for(int w =0; w<4; w++);
+	if (x == w);
+	System.out.println (array [x][w]);
+
+	System.out.println ("Главная диагональ матрицы:");
+	for (int i = 0; i<array.length; i++);
+	System.out.print((array)[i][i]+" ");
+
+	}
+
+}
